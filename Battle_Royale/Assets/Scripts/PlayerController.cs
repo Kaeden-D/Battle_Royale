5891198ea34b8fa96add3bba8bce339a6662035c
Kaeden-D <dalyk3@my.erau.edu>
2025-09-17 10:58:04 -0700

    Add more objects to terrain and add three menu scenes
    page 26

---
+++ Battle_Royale/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Header("Stats")]
+    public float moveSpeed;
+    public float jumpForce;
+
+    [Header("Components")]
+    public Rigidbody rig;
+
+    void Update()
+    {
+        Move();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TryJump();
+        }
+
+    }
+
+    void Move()
+    {
+        // get the input axis
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        // calculate a direction relative to where we're facing
+        Vector3 dir = (transform.forward * z + transform.right * x) * moveSpeed;
+        dir.y = rig.linearVelocity.y;
+        // set that as our velocity
+        rig.linearVelocity = dir;
+    }
+
+    void TryJump()
+    {
+        // create a ray facing down
+        Ray ray = new Ray(transform.position, Vector3.down);
+        // shoot the raycast
+        if (Physics.Raycast(ray, 1.5f))
+        {
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+}
